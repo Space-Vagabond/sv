6a7d47cb3afefa69f18676c866e1f138492dda87
Benoit <gidon@intechinfo.fr>
2014-10-28 16:11:41 +0100

    initialez universe and planet types

--- gv/gv.Tests/Tests.cs
+++ gv/gv.Tests/Tests.cs
@@ -12,13 +12,42 @@ namespace gv.Tests
        public void creates_planet_name_between_6_and_8_char()
         {
             int i;
+            Universe u = new Universe();
             for( i = 0; i < 100; i++ )
             {
-                Planet P = new Planet();
+                Planet P = u.AddPlanet();
                 Console.WriteLine( P._name );
                 Assert.That( P._name.Length <= 10 && P._name.Length >= 8);
             }
 
+        }
+        [Test]
+        public void planet_type_list_works_as_should()
+        {
+            Universe u = new Universe();
+            Assert.AreEqual(u._planetTypes[0], ( "Telluric Silicat" ));
+            Assert.AreEqual(u._planetTypes[1], ( "Telluric Carbon" ));
+            Assert.AreEqual(u._planetTypes[2], ( "Telluric Metal" ));
+            Assert.AreEqual(u._planetTypes[3], ( "Telluric Lava" ));
+            Assert.AreEqual(u._planetTypes[4], ( "Telluric Ice" ));
+            Assert.AreEqual(u._planetTypes[5], ( "Telluric Desert" ));
+            Assert.AreEqual(u._planetTypes[6], ( "Coreless" ));
+            Assert.AreEqual(u._planetTypes[7], ( "Gazeous Hydrogen" ));
+            Assert.AreEqual(u._planetTypes[8], ( "Gazeous Helium" ));
+            Assert.AreEqual(u._planetTypes[9], ( "Chthonian" ));
+        }
+        [Test]
+        public void planet_type_assignement_works()
+        {
+            Universe u = new Universe();
+            int i;
+
+            for( i = 0; i < 100; i++ )
+            {
+                Planet P = u.AddPlanet();
+                Assert.That( u._planetTypes.Contains( P._type ) );
+                Console.WriteLine( P._type );
+            }
         }
     }
 }
--- gv/gv/Planet.cs
+++ gv/gv/Planet.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using ET.FakeText;
 
-
-
 namespace gv
 {
     public class Planet
     {
         public string _name;
-        public static int planetNumber;
+        public static int planetNumber = 0;
+        public string _type;
         Random rand = new Random();
-        public Planet()
+
+        internal Planet( List<string> planetTypes )
         {
             TextGenerator NameGen = new TextGenerator(WordTypes.Name);
             int n =  rand.Next( 6, 9 ); 
@@ -19,6 +19,8 @@ namespace gv
             _name = NameGen.GenerateWord( n );
             _name = _name + "us";
 
+            int t = rand.Next( 0, 10 );
+            _type = planetTypes[t];
         }
     }
 }
