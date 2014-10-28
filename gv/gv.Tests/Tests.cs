2acafe44970584f55422944bd0cee9aae57f1211
Benoit <gidon@intechinfo.fr>
2014-10-28 14:50:22 +0100

    intialized planet creation

---
+++ gv/gv.Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+
+namespace gv.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+       public void creates_planet_name_between_6_and_8_char()
+        {
+            int i;
+            for( i = 0; i < 100; i++ )
+            {
+                Planet P = new Planet();
+                Console.WriteLine( P._name );
+                Assert.That( P._name.Length <= 10 && P._name.Length >= 8);
+            }
+
+        }
+    }
+}
---
+++ gv/gv/Planet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using ET.FakeText;
+
+
+
+namespace gv
+{
+    public class Planet
+    {
+        public string _name;
+        public static int planetNumber;
+        Random rand = new Random();
+        public Planet()
+        {
+            TextGenerator NameGen = new TextGenerator(WordTypes.Name);
+            int n =  rand.Next( 6, 9 ); 
+            
+            _name = NameGen.GenerateWord( n );
+            _name = _name + "us";
+
+        }
+    }
+}
