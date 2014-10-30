c0071a490b1712272b59e6845e09824b90a3698b
Benoit <gidon@intechinfo.fr>
2014-10-30 09:12:04 +0100

    added generate climate

--- gv/gv/Planet.cs
+++ gv/gv/Planet.cs
@@ -9,9 +9,12 @@ namespace gv
         internal string _name;
         internal static int planetNumber = 0;
         internal string _type;
-        internal string _climate;
+        internal int _climate;
         internal string _surface;
         internal string _ressource;
+        internal bool _inhabited;
+        internal string _inhabitantsName;
+
         Random rand = new Random();
 
         internal Planet( List<string> planetTypes )
@@ -30,35 +33,27 @@ namespace gv
         }
         void GeneratePlanetAttributes( int t )
         {
-            switch (t)
-             {
-                 case 0:
-                     
-                     break;
-                 case 1:
-                     
-                     break;
-                 case 2:
-                     
-                     break;
-                 case 3:
-                     
-                     break;
-                 case 4:
-                     break;
-                 case 5:
-                     break;
-                 case 6:
-                     break;
-                 case 7:
-                     break;
-                 case 8:
-                     break;
-                 case 9:
-                     break;
-                 default:
-                     break;
-             };
+            GenerateClimate( t );
+        }
+
+        void GenerateClimate( int t )
+        {
+            if( t == 3 || t == 6 || t == 9 )
+            {
+                _climate = 1;
+            }
+            else if( t == 4 || t == 7 || t == 8 )
+            {
+                _climate = -1;
+            }
+            else
+            {
+                _climate = rand.Next( -1, 2 );
+            }
+        }
+        void GenerateSurface( int t )
+        {
+
         }
     }
 }
