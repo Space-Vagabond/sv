6c2a8c5c45c98c6c1602225db8fb32f5e4a02b72
Nattefrost <escande@intechinfo.fr>
2014-12-19 15:57:57 +0100

    Changed launcher and loader style a bit.

--- gv/Galactic_Vagabond/GameLoader.Designer.cs
+++ gv/Galactic_Vagabond/GameLoader.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameLoader));
             this.Back = new System.Windows.Forms.Button();
             this.LoadSelected = new System.Windows.Forms.Button();
             this.Games = new System.Windows.Forms.ListBox();
@@ -36,54 +37,72 @@
             // 
             // Back
             // 
-            this.Back.Location = new System.Drawing.Point(201, 33);
+            this.Back.BackColor = System.Drawing.Color.Black;
+            this.Back.Font = new System.Drawing.Font("Orator", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Back.ForeColor = System.Drawing.Color.Chartreuse;
+            this.Back.Location = new System.Drawing.Point(234, 33);
             this.Back.Name = "Back";
-            this.Back.Size = new System.Drawing.Size(75, 23);
+            this.Back.Size = new System.Drawing.Size(87, 23);
             this.Back.TabIndex = 0;
             this.Back.Text = "Back";
-            this.Back.UseVisualStyleBackColor = true;
+            this.Back.UseVisualStyleBackColor = false;
             this.Back.Click += new System.EventHandler(this.Back_Click);
             // 
             // LoadSelected
             // 
+            this.LoadSelected.BackColor = System.Drawing.Color.Black;
             this.LoadSelected.Enabled = false;
-            this.LoadSelected.Location = new System.Drawing.Point(201, 129);
+            this.LoadSelected.Font = new System.Drawing.Font("Orator", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadSelected.ForeColor = System.Drawing.Color.Chartreuse;
+            this.LoadSelected.Location = new System.Drawing.Point(234, 129);
             this.LoadSelected.Name = "LoadSelected";
-            this.LoadSelected.Size = new System.Drawing.Size(75, 23);
+            this.LoadSelected.Size = new System.Drawing.Size(87, 23);
             this.LoadSelected.TabIndex = 1;
             this.LoadSelected.Text = "Load";
-            this.LoadSelected.UseVisualStyleBackColor = true;
+            this.LoadSelected.UseVisualStyleBackColor = false;
             this.LoadSelected.Click += new System.EventHandler(this.LoadSelected_Click);
             // 
             // Games
             // 
+            this.Games.BackColor = System.Drawing.Color.Black;
+            this.Games.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Games.Font = new System.Drawing.Font("Orator", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Games.ForeColor = System.Drawing.Color.White;
             this.Games.FormattingEnabled = true;
-            this.Games.Location = new System.Drawing.Point(12, 20);
+            this.Games.ItemHeight = 15;
+            this.Games.Location = new System.Drawing.Point(14, 20);
             this.Games.Name = "Games";
-            this.Games.Size = new System.Drawing.Size(139, 225);
+            this.Games.ScrollAlwaysVisible = true;
+            this.Games.Size = new System.Drawing.Size(161, 45);
             this.Games.TabIndex = 2;
             this.Games.SelectedValueChanged += new System.EventHandler(this.Games_SelectedValueChanged);
             // 
             // Msg
             // 
             this.Msg.AutoSize = true;
-            this.Msg.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Msg.ForeColor = System.Drawing.Color.Red;
-            this.Msg.Location = new System.Drawing.Point(147, 113);
+            this.Msg.BackColor = System.Drawing.Color.Transparent;
+            this.Msg.Font = new System.Drawing.Font("Orator", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Msg.ForeColor = System.Drawing.Color.Orange;
+            this.Msg.Location = new System.Drawing.Point(201, 89);
             this.Msg.Name = "Msg";
-            this.Msg.Size = new System.Drawing.Size(145, 13);
+            this.Msg.Size = new System.Drawing.Size(139, 19);
             this.Msg.TabIndex = 3;
-            this.Msg.Text = "Choose a game to load !";
+            this.Msg.Text = "Choose a game";
             // 
             // GameLoader
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(352, 261);
             this.Controls.Add(this.Msg);
             this.Controls.Add(this.Games);
             this.Controls.Add(this.LoadSelected);
             this.Controls.Add(this.Back);
+            this.Font = new System.Drawing.Font("Orator", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "GameLoader";
             this.Text = "GameLoader";
             this.ResumeLayout(false);
