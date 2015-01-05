2f0eb34e94bef4cab55f1115f393e6aa90a7e38c
Benot Gidon <gidon@intechinfo.fr>
2015-01-05 15:16:58 +0100

    aded tech classes, plaer's fiels for techs and properties likned to them,
    todo: add funtions for player then gui

--- gv/gv/Player.cs
+++ gv/gv/Player.cs
@@ -18,6 +18,17 @@ namespace gv
         int _remainingSteps;
         Universe _universe;
         Dictionary<string, int> _ressources = new Dictionary<string,int>();
+        bool _gazExtractor;
+        bool _gemsExtractor;
+        bool _plutoniumExtractor;
+        bool _hydrogenEngine;
+        bool _heliumEngine;
+        bool _plutoniumEngine;
+        bool _factoryUpgrade;
+        bool _radar;
+        bool _diplomacy;
+        bool _workers;
+        bool _slavery;
 
         
        public Player(Universe u)
@@ -27,6 +38,19 @@ namespace gv
             _speed = 4000;
             _canMove = true;
             _remainingSteps = _speed;
+
+            _gazExtractor = false;
+            _gemsExtractor = false;
+            _plutoniumExtractor = false;
+            _hydrogenEngine = false;
+            _heliumEngine = false;
+            _plutoniumEngine = false;
+            _factoryUpgrade = false;
+            _radar = false;
+            _diplomacy = false;
+            _workers = false;
+            _slavery = false;
+
             foreach( string s in PlanetAttributes.PlanetRessources )
             {
                 if( s != "none" )
@@ -108,6 +132,50 @@ namespace gv
         {
             get { return _name; }
             set { _name = value; }
+        }
+        public bool GazExtractor
+        {
+            get { return _gazExtractor; }
+        }
+        public bool GemsExtractor
+        {
+            get { return _gemsExtractor; }
+        }
+        public bool PlutoniumExtractor
+        {
+            get { return _plutoniumExtractor; }
+        }
+        public bool HydrogenEngine
+        {
+            get { return _hydrogenEngine; }
+        }
+        public bool HeliumEngine
+        {
+            get { return _heliumEngine; }
+        }
+        public bool PlutoniumEngine
+        {
+            get { return _plutoniumEngine; }
+        }
+        public bool FactoryUpgrade
+        {
+            get { return _factoryUpgrade; }
+        }
+        public bool Radar
+        {
+            get { return _radar; }
+        }
+        public bool Diplomacy
+        {
+            get { return _diplomacy; }
+        }
+        public bool Workers
+        {
+            get { return _workers; }
+        }
+        public bool Slavery
+        {
+            get { return _slavery; }
         }
     }
 }
---
+++ gv/gv/Tech.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gv
+{
+    public class Tech
+    {
+        static readonly Dictionary<string,string> Techs = new Dictionary<string, string>()
+       {
+            {"Gaz Extractor","Allows you to extract Helium and Hydrogen"},
+            {"Gems Extractor","Allows you to extract Gems"},
+            {"Plutonium Extractor","Allows you to extract Plutonium"},
+            {"Hydrogen Engine","Increases your max speed to 10"},
+            {"Helium Engine","Increases your max speed to 15"},
+            {"Plutonium Engine","Increases your max speed to 20"},
+            {"Factory Upgrade", "Multiplies your income by 1.5"},
+            {"Radar","Allows you to know where are planets in Overview"},
+            {"Diplomacy","Aliens are now your friends they help for you, adds 10% income for inhabited planets"},
+            {"Workers","Aliens are now your workers they work for you, adds 20% income for inhabited planets"},
+            {"Slavery", "Aliens are now your slaves, you force them to work for you, adds 50% income for inhabited planets"}
+        };
+    }
+}
